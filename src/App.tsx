d2efc2c48f24c51038cefc2c9b4a8b7df019b8c7
praneeth <gp.gunturu@gmail.com>
2025-08-23 21:48:30 -0500

    added api urls in config file and creating api endpoints to fetch the weather

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,30 @@ import { ThemeProvider } from './components/theme-provider'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Dashboard from './pages/Dashboard'
 import City from './pages/City'
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 function App() {
 
     return (
-        <BrowserRouter>
-            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-                <Layout>
-                    <Routes>
-                        <Route path='/' element={<Dashboard />}></Route>
-                        <Route path='/city/:cityName' element={<City />}></Route>
-                    </Routes>
-                </Layout>
-            </ThemeProvider>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                    <Layout>
+                        <Routes>
+                            <Route path='/' element={<Dashboard />}></Route>
+                            <Route path='/city/:cityName' element={<City />}></Route>
+                        </Routes>
+                    </Layout>
+                </ThemeProvider>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     )
 }
 
