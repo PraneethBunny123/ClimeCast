6a9c4bbf14533fd8398aa93836fef8feca12c511
praneeth <gp.gunturu@gmail.com>
2025-08-24 11:53:11 -0500

    added custom options to the react query. Added the error state to display an alert message. Wrote query functions to fetch based the weatherAPI class instance and checking to see if we get the valid reponse from the api calls

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,19 @@
 import LoadingSkeleton from "@/components/LoadingSkeleton";
 import { Button } from "@/components/ui/button";
 import { useGeoLocation } from "@/hooks/useGeoLocation";
-import { RefreshCcw } from "lucide-react";
+import { AlertTriangle, MapPin, RefreshCcw } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { useReverseGeocodeQuery } from "@/hooks/useWeather";
+import { data } from "react-router-dom";
 
 export default function Dashboard() {
     const {coordinates, error: locationError, isLoading: locationLoading, getLocation} = useGeoLocation()
 
     console.log(coordinates);
+
+    const locationQuery = useReverseGeocodeQuery(coordinates)
+    console.log(JSON.parse(JSON.stringify(locationQuery)))
+
 
     function handleRefreshButton() {
         getLocation()
@@ -14,6 +21,37 @@ export default function Dashboard() {
 
     if(locationLoading) {
         return <LoadingSkeleton />
+    }
+
+    if(locationError) {
+        return (
+            <Alert variant="destructive">
+                <AlertTriangle className="h-4 w-4"/>
+                <AlertTitle>Location Error</AlertTitle>
+                <AlertDescription className="flex flex-col gap-4">
+                    <p>{locationError}</p>
+                    <Button onClick={getLocation} variant="outline" className="w-fit">
+                        <MapPin className="mr-2 h-4 w-4" />
+                        Enable Location
+                    </Button>
+                </AlertDescription>
+            </Alert>
+        )
+    }
+
+    if(!coordinates) {
+        return (
+            <Alert variant="destructive">
+                <AlertTitle>Location Required</AlertTitle>
+                <AlertDescription className="flex flex-col gap-4">
+                    <p>Please enable location access to see your local weather.</p>
+                    <Button onClick={getLocation} variant="outline" className="w-fit">
+                        <MapPin className="mr-2 h-4 w-4" />
+                        Enable Location
+                    </Button>
+                </AlertDescription>
+            </Alert>
+        )
     }
 
     return (
