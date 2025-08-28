f634b843f3e4091ca951c082930cb7ed24cc832b
praneeth <gp.gunturu@gmail.com>
2025-08-28 12:31:31 -0500

    Logo changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,20 @@ import { Link } from "react-router-dom";
 import { ModeToggle } from "./toggleMode";
 import CitySearch from "./CitySearch";
 import UnitsToggle from "./UnitsToggle";
+import { useTheme } from "./theme-provider";
 
 export default function Header() {
+    const {theme, setTheme} = useTheme()
+    const isDark = theme === "dark"
+
     return (
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
 
             {/* Link to home page */}
             <div className="container mx-auto flex h-16 items-center justify-between px-4">
-                <Link to="/">ClimeCast</Link>
+                <Link to="/">
+                    <img src={!isDark ? "logo.png" : "darklogo.png"} className="h-22"/>
+                </Link>
                 <div className="flex gap-4">
                     {/* Search */}
                     <CitySearch />
